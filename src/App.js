90389cc4b76cc03e9907800745a1a4f9a32fe655
Hazri Jordan <hazrijordan@gmail.com>
2017-12-15 23:30:33 +0800

    Completed quote generator and modified entry array in webpack.config to remove WS disconnected error

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Todo from "./Todo";
+import Quote from "./Quote";
 
 const App = () => (
     <div>
         <Todo />
+        <Quote />
     </div>
 );
 
---
+++ src/Quote.js
@@ -0,0 +1,44 @@
+import React from "react";
+import axios from "axios";
+
+class Quote extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            quote: '',
+            author: '',
+            link: '',
+        }
+    }
+
+    componentDidMount() {
+        axios.get('http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en')
+            .then((response) => {
+                this.setState({
+                    quote: response.data.quoteText,
+                    author: response.data.quoteAuthor,
+                    link: response.data.quoteLink,
+                })
+    
+            })
+            // .catch( error => {
+            //     console.log('Error fetching and parsing data', error);
+            // });
+    }
+    
+
+
+    render() {
+    
+        return (
+            <div className="quote">
+                <q>{this.state.quote}</q>
+                <p>-{this.state.author}</p>
+                <p>{this.state.link}</p>
+            </div>
+        );
+    }
+}
+
+export default Quote;
+    
