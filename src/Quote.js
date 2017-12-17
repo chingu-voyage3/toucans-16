6b4dcedc7e8bc53a2a203418bab63f736a0b1c4c
Ahmed Zakir <ahmadmzak@gmail.com>
2017-12-17 22:35:58 +0500

    refactored Quotes and deleted TodoButton.js

--- src/Quote.js
+++ src/Quote.js
@@ -2,45 +2,27 @@ import React from "react";
 import axios from "axios";
 
 class Quote extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            baseURL: 'https://talaikis.com/api/quotes/random/',
-            quote: '',
-            author: '',
-            // link: '',
-        }
-    }
-
+    state = {
+        quote: "",
+        author: ""
+    };
     componentDidMount() {
-        axios.get(this.state.baseURL)
-            .then((response) => {
-                this.setState({
-                    quote: response.data.quote,
-                    author: response.data.author,
-                    // link: response.data.quoteLink,
-                })
-                // .bind(this);
-    
-            })
-            // .catch( error => {
-            //     console.log('Error fetching and parsing data', error);
-            // });
+        const baseURL = "https://talaikis.com/api/quotes/random/";
+        axios.get(baseURL).then(response => {
+            this.setState({
+                quote: response.data.quote,
+                author: response.data.author
+            });
+        });
     }
-    
-
-
     render() {
-    
         return (
             <div className="quote">
                 <q>{this.state.quote}</q>
-                <p>-{this.state.author}</p>
-                {/* <p>{this.state.link}</p> */}
+                <p>{this.state.author}</p>
             </div>
         );
     }
 }
 
 export default Quote;
-    
