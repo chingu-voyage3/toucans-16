4a16fc03d642c2dabe4d880d4cb40f0e31cacdd7
Ahmed Zakir <ahmadmzak@gmail.com>
2017-12-19 18:35:53 +0500

    changed React.Component to Component

--- src/Quote.js
+++ src/Quote.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { Component } from "react";
 import axios from "axios";
 
-class Quote extends React.Component {
+class Quote extends Component {
     state = {
         quote: "",
         author: ""
