81fab2876acb3c38281736eaa9540d2b931261c9
Hazri Jordan <hazrijordan@gmail.com>
2017-12-21 18:41:27 +0800

    added random background image

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
-import Quote from "./Quote";
+import Quote from "./Quote/Quote";
 import Todo from "./Todo/Todo";
+import './Wallpaper.css';
 
 const App = () => (
     <div>
