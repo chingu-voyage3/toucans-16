7e9e08679f9559c7976ad3189898fb103b8b836c
Ahmed Zakir <ahmadmzak@gmail.com>
2017-12-23 23:57:13 +0500

    implemented sounds

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import Quote from "./Quote";
 import Todo from "./Todo/Todo";
 import Links from "./Links/Links";
+import Sounds from "./Sounds/Sounds";
 
 const App = () => (
     <div>
         <Todo />
         <Quote />
         <Links />
+        <Sounds />
     </div>
 );
 
