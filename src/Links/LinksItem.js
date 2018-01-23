46f308881004e9d0853c830a61c4e78a23d18727
Ahmed <ahmadmzak@gmail.com>
2018-01-23 08:34:23 +0300

    updated link icon requests

--- src/Links/LinksItem.js
+++ src/Links/LinksItem.js
@@ -22,7 +22,7 @@ class LinksItem extends Component {
             >
                 <div className="links__item__link">
                     <img
-                        src={`http://grabicon.com/icon?domain=${
+                        src={`https://grabicon.com/icon?origin=https://chingu-voyage3.github.io/toucans-16&domain=${
                             this.props.url
                         }`}
                         alt={`${this.props.name} icon`}
