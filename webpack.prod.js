ee4a5473fa4e310227adfe2e7b2d210b26d7f853
Ahmed <ahmadmzak@gmail.com>
2018-02-01 06:10:17 +0500

    added code to minify styles.css

--- webpack.prod.js
+++ webpack.prod.js
@@ -13,7 +13,9 @@ module.exports = merge(common, {
 			test: /\.css$/,
         		use: ExtractTextPlugin.extract({
           			fallback: "style-loader",
-          			use: "css-loader"
+          			use: [
+                        { loader: "css-loader", options: { minimize: true } }
+                    ]
         		})
 		}
 	]
